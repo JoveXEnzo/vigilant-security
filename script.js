556d44d19c2ae1f761ef1a1f97a39c99d30394fe
JoveXEnzo <jovanniemediano@icloud.com>
2026-01-30 09:22:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function loadHTMLBlock(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
 
+// Load navbar and footer into placeholders
+loadHTMLBlock("navbar", "navbar.html");
+loadHTMLBlock("footer", "footer.html");
